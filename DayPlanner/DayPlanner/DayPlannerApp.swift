da8d7d54127dcd93820d361913ba93741ac47370
Daksh Gupta <daksh.gupta@zscaler.com>
2026-07-11 20:57:51 +0530

    Initial commit - Add DayPlanner Xcode project

---
+++ DayPlanner/DayPlanner/DayPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayPlannerApp.swift
+//  DayPlanner
+//
+//  Created by Daksh Gupta on 11/07/26.
+//
+
+import SwiftUI
+
+@main
+struct DayPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
